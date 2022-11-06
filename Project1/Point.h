de22a5067041ff888b610038377f1456a1fb873f
Elad Fixler <efixler@g.jct.ac.il>
2022-11-06 10:06:12 +0200

    add Point

---
+++ Project1/Point.h
@@ -0,0 +1,8 @@
+#pragma once
+class Point
+{
+public:
+	int i, j;
+	Point(int a, int b) : i(a), j(b) {};
+};
+
---
+++ Project1/Source.cpp
@@ -0,0 +1,7 @@
+#include<iostream> 
+#include"Point.h"
+using namespace std;
+int main() {
+	Point p(1, 2);
+	cout << p.i << " " << p.j;
+}
